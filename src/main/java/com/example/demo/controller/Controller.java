ac298786f6b8c8a3ae9c8d2eb2f25c4551d27296
finla <finlana7642@gmail.com>
2023-05-22 12:59:17 +0530

    second commit

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,2 +1,17 @@
-package com.example.demo.controller;public class Controller {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/login")
+    public String log(){
+        return "Welcome to login page";
+    }
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "Welcome to demo session";
+    }
 }
